1d0c6149e1bb5655cf3cd25aa8e6f399e66a683d
bgreene <bgreene@sandia.gov>
2019-07-25 13:58:08 -0600

    integrate Request into example

--- examples/form/form.py
+++ examples/form/form.py
@@ -1,24 +1,14 @@
 from heorot.heorot import Hall
-from heorot.utils import match_path
+from heorot.utils import match_path, parse_query, sanitize
 
 # no additional paths: ^user/[^/]*/update/?$
-TEMPLATE_DIR = "template/"
-STATIC_DIR = "static/"
+# TEMPLATE_DIR = "template/"
+# STATIC_DIR = "static/"
 
 class Basic(Hall):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.inmemory = {"names":[]}
-
-    def retrieve(self,tname):
-        with open(TEMPLATE_DIR+tname) as r:
-            t = r.read()
-        return t
-
-    def static(self,tname):
-        with open(STATIC_DIR+tname) as r:
-            t = r.read()
-        return t
 
     def homepage(self):
         s =  self.retrieve("home-form.html")
@@ -30,37 +20,32 @@ class Basic(Hall):
     def handle_home_form(self, qd):
         self.inmemory["names"].append((qd["firstname"], qd["lastname"]))
         #redirect to home
-        s, _, __  =self.handle('', {})
+        s, _, __  =self.handle('', {},"")
         return s
 
     def nextpage(self):
-        s =  """
-        <h2>next</h2><table>{}</table>
-        <a href="/">home</a>
-        """.format("\n".join(["<tr><td>{}</td><td>{}</td></tr>".format(i[0], i[1]) for i in self.inmemory["names"]]))
+        rows = "\n".join(["<tr><td>{}</td><td>{}</td></tr>".format(sanitize(i[0]),
+                                                                   sanitize(i[1])) for i in self.inmemory["names"]])
+        s = self.view(self.retrieve("show-all.html"), {"rows": rows})
         d = {"content":s}
         template = self.retrieve("header.html")
         return self.view(template, d)
 
-    def handle(self, path, query):
+    def handle(self, request):
         s = ""
         status = '200'
         headers = {}
-        if match_path("^/?$", path):
-            print("match home")
+        if match_path("^/?$", request.path):
             s = self.homepage()
-        elif match_path("next/?$", path):
-            print("match next")
+        elif match_path("next/?$", request.path):
             s = self.nextpage()
-        elif match_path("handle-home-form/?$",path):
-            print("match form")
-            s = self.handle_home_form(query)
+        elif match_path("handle-home-form/?$",request.path):
+            s = self.handle_home_form(parse_query(request.content))
         # match css
-        elif match_path(".*.css", path):
-            s = self.static(path.split("/")[-1])
+        elif match_path(".*.css", request.path):
+            s = self.static(request.path.split("/")[-1])
             headers["Content-Type"] = "text/css"
         else:
-            print("did not match {}".format(path))
             status = '404'
         return s, status, headers
 
