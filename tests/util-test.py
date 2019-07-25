59b60af129ca2d1975d1f59e5328ff99dc9a85d2
bgreene <bgreene@sandia.gov>
2019-07-25 13:57:49 -0600

    more util testing

--- tests/util-test.py
+++ tests/util-test.py
@@ -1,5 +1,10 @@
-from heorot.utils import _test_match
+from heorot.utils import _test_match, sanitize
 
 _test_match("^user/{name}/{action}/?$","user/ben/update/")
 _test_match("^.*/style.css","/next/style.css")
 _test_match("^/?$","/")
+
+print(sanitize('<script>function f(){alert("xss");}window.onload=f;</script>'))
+print(sanitize('<<script>function f(){alert("xss");}window.onload=f;<</script>'))
+print(sanitize('<b>INJECT</b>'))
+print(sanitize('<!--'))
