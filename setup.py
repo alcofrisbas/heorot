cbf97888a1954eaea8b57c13b2de4c646db85b0d
bgreene <bgreene@sandia.gov>
2019-07-29 17:09:41 -0600

    added create script

--- setup.py
+++ setup.py
@@ -2,4 +2,9 @@ import setuptools
 setuptools.setup(name='heorot',
       version='0.1',
       packages=setuptools.find_packages(),
-      )
+      entry_points={
+        "console_scripts": [
+            'heorot-create = heorot.create:main'
+        ]
+      }
+    )
